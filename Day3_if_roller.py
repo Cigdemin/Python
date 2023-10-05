21d5ae94315a7d82e4c7e75c2545b73de16ea50c
cigdemin <cigmenci@hotmail.com>
2023-10-05 11:59:35 -0500

    code block 3 for BMI calculation added

--- Day3_if_roller.py
+++ Day3_if_roller.py
@@ -21,7 +21,37 @@ number = int(input())
 
 # Write your code below this line 👇
 modulo = number % 2
+#if the number divided by 2 you will see result 0
+#if the number is not divided by 2 you will see other number than 0 
 if modulo == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
+
+#Code block 3 for BMI calculation
+# Enter your height in meters e.g., 1.55
+height = float(input())
+# Enter your weight in kilograms e.g., 72
+weight = int(input())
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+BMI = weight / height ** 2
+if BMI <= 18.5:
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif 18.5 < BMI < 25:
+  print(f"Your BMI is {BMI}, you have a normal weight.")
+elif 25 <= BMI < 30:
+  print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif 30 <= BMI < 35:
+  print(f"Your BMI is {BMI}, you are obese.")
+else:
+  print(f"Your BMI is {BMI}, you are clinically obese.")
+
+
+#Under 18.5 they are underweight
+#Over 18.5 but below 25 they have a normal weight
+#Equal to or over 25 but below 30 they are slightly overweight
+#Equal to or over 30 but below 35 they are obese
+#Equal to or over 35 they are clinically obese.
+
