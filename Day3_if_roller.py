094a88722f172d1255e2ff5d676a3869f45c0ed5
cigdemin <cigmenci@hotmail.com>
2023-10-05 11:09:17 -0500

    day3 if cond code added

---
+++ Day3_if_roller.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+  print("You can ride the roller coaster")
+else:
+  print("Sorry you have to be taller before you can ride ")
+  
+#If you want to check the value is equal for some exact number then you have to give == (double equal sign) it means that you just want to check if the value input is equal to your request or not. If you just put one = sign then it will give error
