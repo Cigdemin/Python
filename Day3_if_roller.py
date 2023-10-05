1bd288ddbf95e35a8e54fd7973422fa2f2688989
cigdemin <cigmenci@hotmail.com>
2023-10-05 11:24:40 -0500

    modulo added

--- Day3_if_roller.py
+++ Day3_if_roller.py
@@ -5,4 +5,23 @@ if height >= 120:
 else:
   print("Sorry you have to be taller before you can ride ")
   
-#If you want to check the value is equal for some exact number then you have to give == (double equal sign) it means that you just want to check if the value input is equal to your request or not. If you just put one = sign then it will give error
+#If you want to check the value is equal for some exact number then 
+# you have to give == (double equal sign) it means that you just want to check
+# if the value input is equal to your request or not. 
+# If you just put one = sign then it will give error
+
+
+#Code Block 2 for calculating modulo: 
+# it means that if you devided number with other if the substraction has remainder
+# then modulo shows you the number remainder and you use % sign for that)
+
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+modulo = number % 2
+if modulo == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
