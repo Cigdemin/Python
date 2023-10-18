880214dd7d7fed8177a89677679ed457405bddba
cigdemin <cigmenci@hotmail.com>
2023-10-18 13:56:24 -0500

    calculator logo added

--- Day10_calculator.py
+++ Day10_calculator.py
@@ -16,22 +16,24 @@ operations = {
   "*":multiply,
   "/":divide
 }
+from calculator_art import logo
 def calculator():
-  num1 = int(input("What is the first number? : "))
-  for symbol in operations:
-    print(symbol)
-  continue_calculation = True
-  while continue_calculation == True:
-    operation_symbols = input("Pick an operation from the line above: ")
-    num2 = int(input("What is the second number? : "))
-    calculation_function = operations[operation_symbols]
-    answer = calculation_function(num1,num2)
-    print(f"{num1} {operation_symbols} {num2} = {answer}")
-    response_continue = input(f"Type 'y' to continue with the {answer} or type 'n' to start a new calculation: ")
-    if "y" in response_continue :
-      num1 = answer
-    else:
-      continue_calculation = False
-      calculator()
+    print(logo)
+    num1 = int(input("What is the first number? : "))
+    for symbol in operations:
+        print(symbol)
+    continue_calculation = True
+    while continue_calculation == True:
+        operation_symbols = input("Pick an operation from the line above: ")
+        num2 = int(input("What is the second number? : "))
+        calculation_function = operations[operation_symbols]
+        answer = calculation_function(num1,num2)
+        print(f"{num1} {operation_symbols} {num2} = {answer}")
+        response_continue = input(f"Type 'y' to continue with the {answer} or type 'n' to start a new calculation: ")
+        if "y" in response_continue :
+            num1 = answer
+        else:
+            continue_calculation = False
+            calculator()
 calculator()
   
