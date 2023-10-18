bcb3170be5496656f9c02dd5de14a945d5287ce2
cigdemin <cigmenci@hotmail.com>
2023-10-18 12:50:22 -0500

    docstring information added

--- Day10.py
+++ Day10.py
@@ -1,9 +1,10 @@
 #Fuctions with outputs
 
 def format_name(f_name,l_name):
-  formated_f_name = f_name.title()
-  formated_l_name = l_name.title() 
-  return f"{formated_f_name} {formated_l_name}"
+    """This function formats input in title letters""" #this information giving the fiction document string when the time you are choosind it.
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title() 
+    return f"{formated_f_name} {formated_l_name}"
 print(format_name("CiGDem","caliskan"))
 
   
